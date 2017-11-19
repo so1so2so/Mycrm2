5b220135ccb7c9d4c6557ede66cce9c9ff11a3d2
so1so2so <1037930435@qq.com>
2017-11-20 00:04:03 +0800

    11.19

---
+++ king_admin/king_showadmin.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+from crm import models
+
+enabled_admins = {}
+
+
+class BaseAdmin(object):
+    list_display = ['id']
+
+
+class CustomerAdmin(BaseAdmin):
+    list_display = [ 'name', 'qq', 'qq_name', 'referral_from', 'source', 'date']
+    # model = models_class
+
+
+class MenuAdmin(BaseAdmin):
+    list_display = ['name', 'url_name', ]
+
+
+class UserAdmin(BaseAdmin):
+    list_display = ['user', 'name', 'roles']
+
+
+# 这个函数传递2个值进去 一个是models的表名,一个是自定义的类名
+def regist(model_class, admin_class=BaseAdmin):
+    # _meta.app_label 是找出app名称 _meta.model_name是找出表名称
+    if model_class._meta.app_label not in enabled_admins:
+        enabled_admins[model_class._meta.app_label] = {}
+        # 原来的是 admin.site.register(models.Customer, CustomerAdmin)
+        # 相当于把CustomerAdmin这个类新加一个model的属性,把这2个属性传递进去
+    if admin_class:
+        admin_class.model = model_class  # 绑定model 对象和admin 类
+    # 定义数组里面嵌套的数组,类型相当于{app_name:{table_name:admin_class}} admin_class可以找出对应的列
+    enabled_admins[model_class._meta.app_label][model_class._meta.model_name] = admin_class
+    # enabled_admins['crm']['Customer'] = CustomerAdmin
+
+
+regist(models.Customer, CustomerAdmin)
+regist(models.Menu, MenuAdmin)
+regist(models.UserProfile, UserAdmin)
+regist(models.Enrollment)
